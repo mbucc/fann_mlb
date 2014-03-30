922e8206ba8d8f4bccdf883ca746d66c492e151b
Mark Bucciarelli <mkbucc@gmail.com>
2014-03-30 12:57:50 -0400

    No logic change, use simpler contructor for nnet.

--- src/firstplace.c
+++ src/firstplace.c
@@ -2,35 +2,14 @@
 
 int main()
 {
-    const unsigned int num_input = 93;
-    const unsigned int num_output = 1;
-    const unsigned int num_layers = 4;
-    const unsigned int num_neurons_hidden_layer1 = 60;
-    const unsigned int num_neurons_hidden_layer2 = 20;
+    const unsigned int layers_n = 4;
+    const unsigned int nodes_per_layer[] = {93, 60, 20, 1};
+
     const float desired_error = (const float) 0.0000005;
     const unsigned int max_epochs = 10000;
     const unsigned int epochs_between_reports = 1000;
 
-    struct fann *ann = 0;
-    if (num_layers == 3)
-	    ann = fann_create_standard(
-			    3
-			    , num_input
-			    , num_neurons_hidden_layer1
-			    , num_output
-			    );
-    else if (num_layers == 4)
-	    ann = fann_create_standard(
-			    4
-			    , num_input
-			    , num_neurons_hidden_layer1
-			    , num_neurons_hidden_layer2
-			    , num_output
-			    );
-    else {
-	    fputs("Invalid number of layers.", stderr);
-	    exit(1);
-    }
+    struct fann *ann = fann_create_standard_array(layers_n, nodes_per_layer);
 
     fann_set_activation_function_hidden(ann, FANN_SIGMOID_SYMMETRIC);
     fann_set_activation_function_output(ann, FANN_SIGMOID_SYMMETRIC);
